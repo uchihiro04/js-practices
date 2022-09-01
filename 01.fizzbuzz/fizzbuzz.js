c866c054f463b6cec6fa636643836c10c9369c6b
uchihiro <tohchi4122@gmail.com>
2022-09-01 21:20:42 +0900

    5の倍数のとき、Buzzを表示
    5の倍数のとき、数値の代わりにBuzzを表示するように変更しました。

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,6 +1,8 @@
 for (let number = 1; number < 21; number++) {
   if (number % 3 === 0) {
     console.log('Fizz');
+  } else if (number % 5 === 0) {
+    console.log('Buzz');
   } else {
     console.log(number);
   }
