06e99e4bd18c8b5f5eceabcbd530fdf73425e246
uchihiro <tohchi4122@gmail.com>
2022-09-02 14:38:36 +0900

    セミコロンを削除
    JavaScript Standard Styleに合わせるため、行末についていたセミコロンを削除しました。

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -2,10 +2,10 @@ for (let number = 1; number < 21; number++) {
   if (number % 3 === 0 && number % 5 === 0) {
     console.log('FizzBuzz')
   } else if (number % 3 === 0) {
-    console.log('Fizz');
+    console.log('Fizz')
   } else if (number % 5 === 0) {
-    console.log('Buzz');
+    console.log('Buzz')
   } else {
-    console.log(number);
+    console.log(number)
   }
 }
