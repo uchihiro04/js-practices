9d41383b9c0a0b97bcce659a45cb7fe50d99a6f3
uchihiro <tohchi4122@gmail.com>
2022-10-12 16:26:49 +0900

    土曜日、日曜日を表す定数をそれぞれ追加
    マジックナンバーを削除するため、土曜日・日曜日を表す定数をそれぞれ追加しました。

--- 02.calendar/calender.js
+++ 02.calendar/calender.js
@@ -9,15 +9,17 @@ const firstDay = date.startOf('month')
 const lastDay = date.endOf('month')
 
 const dayMargin = 3
+const sunday = 7
+const saturday = 6
 
 console.log(date.toFormat('      M月 yyyy'))
 console.log('日 月 火 水 木 金 土')
 
-if (firstDay.weekday !== 7) process.stdout.write(' '.repeat(dayMargin * firstDay.weekday))
+if (firstDay.weekday !== sunday) process.stdout.write(' '.repeat(dayMargin * firstDay.weekday))
 
 for (let day = firstDay.day; day <= lastDay.day; day++) {
   const dayOfWeek = firstDay.plus({ days: day - 1 }).weekday
   process.stdout.write(day.toString().padStart(2) + ' ')
-  if (dayOfWeek === 6 && day !== lastDay.day) process.stdout.write('\n')
+  if (dayOfWeek === saturday && day !== lastDay.day) process.stdout.write('\n')
   if (day === lastDay.day) process.stdout.write('\n\n')
 }
