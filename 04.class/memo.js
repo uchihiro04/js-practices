90f64d0b938c53306a1a0f97643aafe970ab2f95
uchihiro <tohchi4122@gmail.com>
2022-10-21 16:53:36 +0900

    一部コードを関数化
    メモの作成およびJSONファイルへの書き込みについて関数化しました。

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -3,31 +3,37 @@
 const fs = require("fs");
 const jsonPath = "db/memo.json";
 
-process.stdin.setEncoding("utf8");
-
-const lines = [];
-const memoText = {};
-
-let reader = require("readline").createInterface({
-  input: process.stdin,
-});
-
 if (!fs.existsSync(jsonPath)) {
   fs.writeFileSync(jsonPath, '{"memos": []}');
 }
 const jsonFile = fs.readFileSync(jsonPath, "utf-8");
 const memosText = JSON.parse(jsonFile);
 
-reader.on("line", (line) => {
-  lines.push(line);
-});
+function createMemo() {
+  process.stdin.setEncoding("utf8");
+  const reader = require("readline").createInterface({
+    input: process.stdin,
+  });
+  const lines = [];
+  const memoText = {};
 
-reader.on("close", () => {
-  memoText.memo = lines.join("\n");
-  memosText.memos.push(memoText);
+  reader.on("line", (line) => {
+    lines.push(line);
+  });
+
+  reader.on("close", () => {
+    memoText.memo = lines.join("\n");
+    memosText.memos.push(memoText);
+    writeMemo(memosText);
+  });
+}
+
+function writeMemo(memosText) {
   const jsonText = JSON.stringify(memosText);
   fs.writeFile(jsonPath, jsonText, (err) => {
     if (err) throw err;
     console.log("---書き込みが完了しました---");
   });
-});
+}
+
+createMemo();
