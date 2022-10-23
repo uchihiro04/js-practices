4a1428e024847d658ff93684c9ebea3037dec95b
uchihiro <tohchi4122@gmail.com>
2022-10-23 20:29:15 +0900

    メモの参照機能を実装
    -rオプションでメモを参照する機能を実装しました。

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -2,14 +2,20 @@
 
 const fs = require("fs");
 const argv = require("minimist")(process.argv.slice(2));
+const { v4: uuidv4 } = require("uuid");
 const jsonPath = "db/memo.json";
-
 const memosText = readMemo();
 if (argv.l) {
   allMemo();
+} else if (argv.r) {
+  referenceMemo();
 } else {
   createMemo();
 }
+
+// async function sample() {
+// }
+// sample();
 
 function allMemo() {
   const memos = memosText.memos;
@@ -41,6 +47,8 @@ function createMemo() {
 
   reader.on("close", () => {
     const memosText = readMemo();
+    memoText.id = uuidv4();
+    memoText.firstLine = lines[0];
     memoText.memo = lines.join("\n");
     memosText.memos.push(memoText);
     writeMemo(memosText);
@@ -53,4 +61,27 @@ function writeMemo(memosText) {
     if (err) throw err;
     console.log("---書き込みが完了しました---");
   });
+}
+
+function referenceMemo() {
+  const { Select } = require("enquirer");
+  const choices = [];
+  const memos = memosText.memos;
+  memos.forEach((memo) => {
+    const choice = {};
+    choice.name = memo.memo;
+    choice.value = memo.id;
+    choice.message = memo.firstLine;
+    choices.push(choice);
+  });
+
+  const prompt = new Select({
+    name: "id",
+    message: "Choose a note you want to see:",
+    choices: choices,
+  });
+  prompt
+    .run()
+    .then((answer) => console.log(answer))
+    .catch(console.error);
 }
