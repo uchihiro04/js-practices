2105ba235c000dd2debd0770f994af609102b4de
uchihiro <tohchi4122@gmail.com>
2022-11-01 17:16:19 +0900

    クラス構文に変更
    コードについて、クラス構文に変更しました。

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -1,119 +1,146 @@
 #! /usr/bin/env node
 
-const fs = require("fs");
-const argv = require("minimist")(process.argv.slice(2));
-const { v4: uuidv4 } = require("uuid");
-const jsonPath = "db/memo.json";
-const memosText = readMemo();
-
-if (argv.l) {
-  allMemo();
-} else if (argv.r) {
-  referenceMemo();
-} else if (argv.d) {
-  deleteMemo();
-} else {
-  createMemo();
-}
-
-function allMemo() {
-  const memos = memosText.memoslist;
-  memos.forEach((memo) => {
-    console.log(memo.firstLine);
-  });
-}
-
-function readMemo() {
-  if (!fs.existsSync(jsonPath)) {
-    fs.writeFileSync(jsonPath, '{"memoslist": []}');
+class Command {
+  constructor(option) {
+    this.option = option;
   }
-  const jsonFile = fs.readFileSync(jsonPath, "utf-8");
-  return JSON.parse(jsonFile);
+
+  runMemo() {
+    if (this.option.l) {
+      return new MemoController().index();
+    }
+    if (this.option.r) {
+      return new MemoController().reference();
+    }
+    if (this.option.d) {
+      return new MemoController().delete();
+    }
+    return new MemoController().create();
+  }
 }
 
-function createMemo() {
-  process.stdin.setEncoding("utf8");
-  const reader = require("readline").createInterface({
-    input: process.stdin,
-  });
+class MemoController {
+  constructor() {
+    this.memos = new MemoData();
+  }
 
-  const lines = [];
-  reader.on("line", (line) => {
-    lines.push(line);
-  });
-
-  reader.on("close", () => {
-    const allMemo = readMemo();
-    const memo = {
-      id: uuidv4(),
-      firstLine: lines[0],
-      body: lines.join("\n"),
-    };
-    allMemo.memoslist.push(memo);
-    writeMemo(allMemo);
-    console.log("---書き込みが完了しました---");
-  });
-}
-
-function writeMemo(memosText) {
-  const jsonText = JSON.stringify(memosText);
-  fs.writeFile(jsonPath, jsonText, (err) => {
-    if (err) throw err;
-  });
-}
-
-function createChoices() {
-  const choices = [];
-  const memos = memosText.memoslist;
-  memos.forEach((memo) => {
-    const choice = {
-      name: memo.firstLine,
-      value: memo.id,
-      message: memo.firstLine,
-    };
-    choices.push(choice);
-  });
-  return choices;
-}
-
-function createQuestions() {
-  const questions = [
-    {
-      type: "select",
-      name: "id",
-      message: "Choose a note you want to see:",
-      choices: createChoices(),
-      result() {
-        return this.focused.value;
-      },
-    },
-  ];
-  return questions;
-}
-
-function referenceMemo() {
-  const { prompt } = require("enquirer");
-  const memos = memosText.memoslist;
-
-  (async function () {
-    const questions = createQuestions();
-    const answers = await prompt(questions);
-    const memo = memos.find((memo) => memo.id === answers.id);
-    console.log(memo.body);
-  })();
-}
-
-function deleteMemo() {
-  const { prompt } = require("enquirer");
-  const memos = memosText.memoslist;
-
-  (async function () {
-    const questions = createQuestions();
-    let answers = await prompt(questions);
-    const deletedMemos = memos.filter((memo) => {
-      return memo.id !== answers.id;
+  index() {
+    const memos = this.memos.read().memoslist;
+    const firstLines = [];
+    memos.forEach((memo) => {
+      firstLines.push(memo.firstLine);
     });
-    memosText.memoslist = deletedMemos;
-    writeMemo(memosText);
-  })();
+    console.log(firstLines.join("\n"));
+  }
+
+  create() {
+    const { v4: uuidv4 } = require("uuid");
+    process.stdin.setEncoding("utf8");
+    const reader = require("readline").createInterface({
+      input: process.stdin,
+    });
+
+    const lines = [];
+    reader.on("line", (line) => {
+      lines.push(line);
+    });
+
+    reader.on("close", () => {
+      const memos = this.memos.read();
+      const memo = {
+        id: uuidv4(),
+        firstLine: lines[0],
+        body: lines.join("\n"),
+      };
+      memos.memoslist.push(memo);
+      this.memos.write(memos);
+      console.log("---メモを追加しました---");
+    });
+  }
+
+  reference() {
+    const { prompt } = require("enquirer");
+    const memos = this.memos.read().memoslist;
+    const questions = this.createQuestions(memos);
+
+    (async function () {
+      const answers = await prompt(questions);
+      const memo = memos.find((memo) => memo.id === answers.id);
+      console.log(memo.body);
+    })();
+  }
+
+  delete() {
+    const { prompt } = require("enquirer");
+    const memos = this.memos.read();
+    const questions = this.createQuestions(memos.memoslist);
+
+    async function deleteMemo() {
+      const answers = await prompt(questions);
+      const undeletedMemos = memos.memoslist.filter((memo) => {
+        return memo.id !== answers.id;
+      });
+      memos.memoslist = undeletedMemos;
+      return memos;
+    }
+
+    deleteMemo().then((memos) => {
+      this.memos.write(memos);
+    });
+  }
+
+  createChoices(memos) {
+    const choices = [];
+    memos.forEach((memo) => {
+      const choice = {
+        name: memo.firstLine,
+        value: memo.id,
+        message: memo.firstLine,
+      };
+      choices.push(choice);
+    });
+    return choices;
+  }
+
+  createQuestions(memos) {
+    const questions = [
+      {
+        type: "select",
+        name: "id",
+        message: "Choose a note you want to see:",
+        choices: this.createChoices(memos),
+        result() {
+          return this.focused.value;
+        },
+      },
+    ];
+    return questions;
+  }
 }
+
+class MemoData {
+  constructor() {
+    this.path = "db/memo.json";
+  }
+
+  read() {
+    const fs = require("fs");
+    if (!fs.existsSync(this.path)) {
+      fs.writeFileSync(this.path, '{"memos": []}');
+    }
+    const jsonFile = fs.readFileSync(this.path, "utf-8");
+    return JSON.parse(jsonFile);
+  }
+
+  write(memos) {
+    const fs = require("fs");
+    const jsonMemos = JSON.stringify(memos);
+    fs.writeFile(this.path, jsonMemos, (err) => {
+      if (err) throw err;
+    });
+  }
+}
+
+const argv = require("minimist")(process.argv.slice(2));
+const command = new Command(argv);
+command.runMemo();
